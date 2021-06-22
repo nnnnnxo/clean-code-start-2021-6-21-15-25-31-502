d86d60589c99422e21db8b18523a6df2244df97a
waylon <waylon.ye@oocl.com>
2021-06-23 00:29:23 +0800

    [$6-2] add field for fix primitive obsession

--- src/main/java/com/tw/academy/basic/$6_primitive_obsession/practiceTwo/TimeRange.java
+++ src/main/java/com/tw/academy/basic/$6_primitive_obsession/practiceTwo/TimeRange.java
@@ -1,4 +1,22 @@
 package com.tw.academy.basic.$6_primitive_obsession.practiceTwo;
 
 public class TimeRange {
+    private int startHours;
+    private int endHours;
+
+    public int getStartHours() {
+        return startHours;
+    }
+
+    public void setStartHours(int startHours) {
+        this.startHours = startHours;
+    }
+
+    public int getEndHours() {
+        return endHours;
+    }
+
+    public void setEndHours(int endHours) {
+        this.endHours = endHours;
+    }
 }
