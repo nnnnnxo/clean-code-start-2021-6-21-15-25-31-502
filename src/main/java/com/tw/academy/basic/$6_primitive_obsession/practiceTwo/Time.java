abf5d5e58b9c8d1b41bba8d9a304276517e1967f
waylon <waylon.ye@oocl.com>
2021-06-22 23:34:39 +0800

    [$6-2] add constructor for fix primitive obsession

--- src/main/java/com/tw/academy/basic/$6_primitive_obsession/practiceTwo/Time.java
+++ src/main/java/com/tw/academy/basic/$6_primitive_obsession/practiceTwo/Time.java
@@ -1,7 +1,13 @@
 package com.tw.academy.basic.$6_primitive_obsession.practiceTwo;
 
 public class Time {
-    public Time(String time) {
+    private final String time;
 
+    public Time(String time) {
+        this.time = time;
+    }
+
+    public String getTime() {
+        return time;
     }
 }
